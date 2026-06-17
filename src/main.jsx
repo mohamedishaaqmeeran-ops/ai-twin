df1290aa3053886d976ea7f4dd712da4b99faaf9
unknown <mohamedishaaqmeeran@gmail.com>
2026-06-17 14:01:31 +0530

    add gcp cloud run deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,20 @@ import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App.jsx";
 import "./styles.css";
-
+console.log(
+  "Google Client ID:",
+  import.meta.env.VITE_GOOGLE_CLIENT_ID
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID} >
+    
       <BrowserRouter>
         <App />
+        
       </BrowserRouter>
+      
     </GoogleOAuthProvider>
   </React.StrictMode>
+  
 );
