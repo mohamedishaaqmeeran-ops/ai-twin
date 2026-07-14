7994b56ccb05ae8ef3d3b981347b558210bfb47b
unknown <mohamedishaaqmeeran@gmail.com>
2026-07-14 18:08:54 +0530

    update

--- src/features/products/productSlice.js
+++ src/features/products/productSlice.js
@@ -6,7 +6,7 @@ import {
 
 const API_URL =
   import.meta.env.VITE_API_URL ||
-  "http://localhost:8000/api";
+  "https://twinn-backend.onrender.com/api";
 
 /* =========================================================
    FETCH PRODUCTS
